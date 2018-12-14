a5a56011c7ecd17356289c9a2dceac60671b8133
Ratie263 <rvushe87@smith.edu>
2018-12-13 20:35:04 -0500

    corrections

--- src/main/java/edu/smith/cs/csc212/p10/TState.java
+++ src/main/java/edu/smith/cs/csc212/p10/TState.java
@@ -4,25 +4,17 @@ public enum TState {
 	Player1Turn, // 0
 	Player2Turn, // 1
 	HomePage,
-<<<<<<< HEAD
-// Tie, // 2
-=======
-	// Tie, // 2
->>>>>>> branch 'master' of https://github.com/neamtsabry/Tsoro.git
-	OutOfMoves, MoveTokAround, Player1Win, // 3
+	OutOfMoves, 
+	MoveTokAround, 
+	Player1Win, // 3
 	Player2Win // 4
 	;
 
 	public boolean isPlaying() {
-<<<<<<< HEAD
-		return this == Player1Turn || this == Player2Turn || this == OutOfMoves;
-	}
 
-=======
 		return this == Player1Turn || this == Player2Turn || this==OutOfMoves ;
 	}
 	
->>>>>>> branch 'master' of https://github.com/neamtsabry/Tsoro.git
 	public TMark getMarkForTurn() {
 		if (this == Player1Turn) {
 			return TMark.Player1;
